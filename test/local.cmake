19a4f00375845cdb87abc174f5ce87b520d47009
Uros Stojkovic <stoj@in.tum.de>
2023-12-10 22:02:54 +0100

    remove moderndbs occurrences

--- test/local.cmake
+++ test/local.cmake
@@ -12,4 +12,4 @@ add_executable(tester test/tester.cc ${TEST_CC})
 target_link_libraries(tester grlib gtest gmock Threads::Threads)
 
 enable_testing()
-add_test(moderndbs tester)
+add_test(guided-research tester)
